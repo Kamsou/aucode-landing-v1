f3e34f5bc7981b73ac34f3427f56be6233364d03
Camille Coutens <camille.coutens@gmail.com>
2023-08-31 12:57:01 +0200

    fix: footer & add clipboard

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,32 @@
 import IconAppleStore from "@/assets/icons/IconAppleStore.vue";
 import IconGooglePlay from "@/assets/icons/IconGooglePlay.vue";
 
+const contactButton = ref("Contactez-nous");
+
 const title = ref("Aucode — L'entrainement en ligne pour les développeurs");
 const description = ref(
   "Devenir codeur confiant. Plonge dans un univers d'apprentissage interactif du développement web ! Aucode t'offre des quiz adaptés et un feedback en temps réel pour te transformer en un développeur sûr de ses compétences."
 );
+
+function setContactButtonText(text: string, duration = 2000) {
+  contactButton.value = text;
+
+  if (duration) {
+    setTimeout(() => {
+      contactButton.value = "Contactez-nous";
+    }, duration);
+  }
+}
+
+async function copyEmailToClipboard() {
+  try {
+    await navigator.clipboard.writeText("contact@aucode.tech");
+    setContactButtonText("Email copié !");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+    setContactButtonText("Erreur !");
+  }
+}
 
 useHead({
   title,
@@ -28,11 +50,14 @@ useHead({
       </div>
     </div>
 
-    <p
-      class="absolute top-5 sm:top-7 right-5 sm:right-10 border-solid border-2 border-black border-b-4 rounded-xl text-black transform transition duration-300 ease-in-out py-[10px] px-4 hover:scale-105 text-sm cursor-pointer"
-    >
-      Contactez-nous
-    </p>
+    <div class="w-[150px] absolute top-5 sm:top-7 right-5 sm:right-10">
+      <div
+        class="border-solid border-2 border-black border-b-4 rounded-xl text-black transform transition duration-300 ease-in-out py-[10px] px-4 hover:scale-105 text-sm cursor-pointer text-center"
+        @click="copyEmailToClipboard"
+      >
+        {{ contactButton }}
+      </div>
+    </div>
 
     <div class="sm:h-full flex justify-center items-center">
       <div class="sm:max-w-[800px] mx-auto">
@@ -75,7 +100,7 @@ useHead({
     </div>
 
     <div
-      class="footer:absolute footer:bottom-10 footer:right-10 text-center text-xs sm:text-base sm:text-center sm:pt-0 flex gap-4 items-center footer:justify-end flex-col sm:flex-row pt-10 pb-10 sm:pb-0 bg-[#F2F2F2]"
+      class="footer:absolute footer:bottom-10 footer:right-10 text-center text-xs sm:text-base sm:text-center sm:pt-0 flex gap-4 items-center footer:justify-end flex-col sm:flex-row p-10 footer:p-0 xs:pb-0 bg-[#F2F2F2]"
     >
       <NuxtLink to="/legal" class="hover:underline cursor-pointer">
         Mentions Légales
